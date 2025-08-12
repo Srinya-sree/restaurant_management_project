a4aada5adf7c6c4b1e8e9cad0f034aef8ad1c302
Sri Saranya Chandrapati <srinya1421@gmail.com>
2025-08-12 19:04:44 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
-def menu_items_view(request):
-    menu_items=[
-        {'name':'pizza','price':10.99},
-        {'name':'Burger','price':7.99},
-        {'name':'Salad','price':5.99},
-        {'name':'Pasta','price':8.99},
+from django.shortcuts import render  # Import Django's render function for rendering templates
 
+# View function to display a list of menu items
+def menu_items_view(request):
+    # Hardcoded list of menu items with their name and price
+    menu_items = [
+        {'name': 'Pizza', 'price': 10.99},
+        {'name': 'Burger', 'price': 7.99},
+        {'name': 'Salad', 'price': 5.99},
+        {'name': 'Pasta', 'price': 8.99},
     ]
-    return render(request,'MENU_ITEMS.html',{'MENU_ITEMS':MENU_ITEMS})
+    
+    # Render the 'menu_items.html' template and pass menu_items as context data
+    return render(request, 'menu_items.html', {'menu_items': menu_items})
