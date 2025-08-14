e96aa5258bf340d8f8135bdbbe3ec1d7e588a3c6
Sri Saranya Chandrapati <srinya1421@gmail.com>
2025-08-14 21:34:30 +0530

    31294

--- home/views.py
+++ home/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render  # Import Django's render function for rendering templates
-
-# View function to display a list of menu items
+from django.shortcuts import render
 def menu_items_view(request):
-    # Hardcoded list of menu items with their name and price
-    menu_items = [
-        {'name': 'Pizza', 'price': 10.99},
-        {'name': 'Burger', 'price': 7.99},
-        {'name': 'Salad', 'price': 5.99},
-        {'name': 'Pasta', 'price': 8.99},
-    ]
-    
-    # Render the 'menu_items.html' template and pass menu_items as context data
-    return render(request, 'menu_items.html', {'menu_items': menu_items})
+    try:
+        menu_items=[
+            {'name':'pizza','price':10.99},
+            {'name':'Burger','price':7.99},
+            {'name':'Salad','price':5.99},
+            {'name':'Pasta','price':8.99},
+        ]
+        return render(request,'MENU_ITEMS.html',{'MENU_ITEMS':MENU_ITEMS})
+    except DatabaseError as e:
+        print(f"Database error:{e}")
+        return HttpResponse("Sorry, we are experiencing technical issues. Please try again.",status=500)
+        print(f"Unexpected error:{e}")
+        return HttpResponse("An unexpected error.",status=500)
