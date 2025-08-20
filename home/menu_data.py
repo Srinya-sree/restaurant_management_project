a1c31b1e8111dd3d5ed9f4f8e674421d76131937
Sri Saranya Chandrapati <srinya1421@gmail.com>
2025-08-20 10:02:10 +0530

    35992

---
+++ home/menu_data.py
@@ -0,0 +1,17 @@
+MENU = [
+    {
+        "name":"Margherita Pizza",
+        "description":"Classic cheese and tomato pizza with basil",
+        "price":8.99
+    },
+    {
+        "name":"Pasta Alfredo",
+        "description":"Creamy Alfredo pasta with  parmesan and herbs",
+        "price":12.50
+    },
+    {
+        "name":"Ceaser Salad",
+        "description":"Crisp romain,parmesan,croutons,Caesar dressing",
+        "price":6.75
+    }
+]
