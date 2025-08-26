c4543056c9be72287a8a3b76fb3df6d6ebae0b50
Sri Saranya Chandrapati <srinya1421@gmail.com>
2025-08-26 20:57:30 +0530

    38374

--- home/models.py
+++ home/models.py
@@ -5,6 +5,12 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6,decimal_places=2)
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
      def __str__(self):
         return self.name
         return f"{self.name} - ${self.price}"
+        return f"{self.name}-{self.email}"
