801395d651e28d8f6c520bed9fe137c8d3e0dc04
Sri Saranya Chandrapati <srinya1421@gmail.com>
2025-08-27 21:30:40 +0530

    38375

--- home/views.py
+++ home/views.py
@@ -1,13 +1,22 @@
 from django.conf import settings
 from django.shortcuts import render
-
+from .forms import Form
 def home_view(request):
     restaurant_name = getattr(settings, 'RESTAURANT_NAME', 'My Restaurant')
     return render(request, 'home.html',{'restaurant_name': restaurant_name})
 def about_view(request):
     return render(request, 'about.html')
 def home(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+        else:
+            form = ContactForm()
     context={
+        "form":form,
+        "restaurant_address":settings.RESTAURANT_ADDRESS,
         "restaurant_name":settings.RESTAURANT_NAME
     }
     api_url = "http://127.0.0.1:8000/api/menu/"
