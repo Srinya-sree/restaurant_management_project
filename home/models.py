f73f66a9077a7460e2f9f37b9b3a5550b8d48f3d
Sri Saranya Chandrapati <srinya1421@gmail.com>
2025-09-01 18:57:07 +0530

    40301

--- home/models.py
+++ home/models.py
@@ -2,9 +2,12 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
 class MenuItem(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=120)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6,decimal_places=2)
+    image = models.ImageField(upload_to="menu_images/",blank = True,null=True)
+    def __str__(self):
+        return self.name
 
 class Contact(models.Model):
     name = models.CharField(max_length=100)
@@ -23,3 +26,4 @@ class RestaurantLocation(models.Model):
 
     def __str__(self):
         return f"{self.address},{self.city},{self.state}{self.zip_code}"
+
